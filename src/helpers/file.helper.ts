edbbd582e8a787a4561afe3870b3a18036a5cdad
Rabenandrasana Tafinasoa <tafinasoa35@gmail.com>
2023-09-04 13:11:17 +0300

    testing

--- src/helpers/file.helper.ts
+++ src/helpers/file.helper.ts
@@ -1,9 +1,12 @@
 import path from "path";
 import fs from "fs";
 const tempDirectory = path.resolve(__dirname, "../images/");
-export const getFile = (filename: string) => {
+export const getFileAsBase64 = (filename: string) => {
   const filePath = path.resolve(tempDirectory, filename);
   console.log(filePath);
   const image = fs.readFileSync(filePath, { encoding: "base64" });
   return image;
+};
+export const getFilePath = async (fileName: string) => {
+  return path.resolve(tempDirectory, fileName);
 };
